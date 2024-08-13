efbf99c19e6048bf9d5b410518a47752011e6a8e
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-08-13 18:36:07 +0700

    adding new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,6 +20,13 @@ const projects = [
 		img: require('@/assets/images/Sunnyside agency landing page.png'),
 		url: 'https://abibguardian50.github.io/SunnySide-landingPage/'
 	},
+	{
+		id: 4,
+		title: 'UI Testing Elgeka Website (On Progress)',
+		category: 'Web Application',
+		img: require('@/assets/images/Halaman Utama Sahabat CGI.png'),
+		url: 'https://drive.google.com/drive/folders/1sqT9FmMb703ZJEydCyhrAJ6mUoOX9bUm?usp=sharing'
+	},
 ];
 
 export default projects;
